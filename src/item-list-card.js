c464ddf31e8532b8444953545454076b1dd33be3
Duncan1106 <duncan.zirkel@gmail.com>
2026-01-03 16:14:14 +0100

    removed error logging from earlier testing

--- src/item-list-card.js
+++ src/item-list-card.js
@@ -673,10 +673,7 @@ class ItemListCard extends LitElement {
         'Fehler beim Aktualisieren des Eintrags'
       );
       // Press update button only if amount got changed
-      console.log('update_button_entity:', this.config.update_button_entity);
-      console.log('updates.description:', updates.description);
       if (this.config.update_button_entity) {
-        console.error('Update button pressed');
         await callService(
           this.hass,
           'input_button',
